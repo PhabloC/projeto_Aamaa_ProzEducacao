aba64b6fb00a449aedc65c7cac553d9224a6ecde
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-11-22 08:38:26 -0300

    aparecer e ocultar popup

--- profissionais.js
+++ profissionais.js
@@ -1,21 +1,21 @@
-let inputNome = document.getElementById("iNome");
+let inputName = document.getElementById("iName");
 let nameLabel = document.querySelector('label[for="name"]');
 let inputEmail = document.getElementById("iEmail");
 let inputNumero = document.getElementById("iNumero");
 let formulario = document.querySelector("form");
 
 //Mostrar o popup de campo obrigatório
-inputNome.addEventListener("focus", ()=>{
+inputName.addEventListener("focus", ()=>{
     nameLabel.classList.add("requiredPopup")
 });
 
 //Ocultar popup de campo obrigatório
-inputNome.addEventListener("blur", ()=>{
+inputName.addEventListener("blur", ()=>{
     nameLabel.classList.remove("requiredPopup")
 });
 
 //Validar valor do input
-inputNome.addEventListener("change", (e)=>{
+inputName.addEventListener("change", (e)=>{
     let valor = e.target.value
     if(valor.lenght < 6){
         //valor incorreto
