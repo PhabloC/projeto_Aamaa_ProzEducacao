b971f31caf534b82b6df6a7a70612d4deedc0c75
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-11-23 16:39:12 -0300

    blur

--- profissionais.js
+++ profissionais.js
@@ -2,10 +2,18 @@
 let inputName = document.getElementById("iName");
 let nameLabel = document.querySelector('label[for="name"]');
 let nameHelper = document.getElementById("nameHelper");
+
 //variaveis email
 let inputEmail = document.getElementById("iEmail");
 let emailLabel = document.querySelector('label[for="email"]');
 let emaillHelper = document.getElementById("emailHelper");
+
+//variaveis Submit/inputs para button
+let btnSubmit = document.querySelector('button[type="submit"]');
+let inputsCorretos = {
+    name: false,
+    email: false
+}
 
 function mostrarPopup(input, label){
         //Mostrar o popup de campo obrigatório
@@ -19,12 +27,12 @@ function mostrarPopup(input, label){
     });
 
 }
-function estilizarTnputCorreto(input, helper){
+function estilizarInputCorreto(input, helper){
     helper.classList.remove("visible");
     input.classList.remove("error");
     input.classList.add("correct");    
 }
-function estilizarTnputIncorreto(input, helper){
+function estilizarInputIncorreto(input, helper){
     helper.classList.add("visible");
     input.classList.add("error");
     input.classList.remove("correct");    
@@ -36,15 +44,15 @@ function estilizarTnputIncorreto(input, helper){
 mostrarPopup(inputName, nameLabel)
 
 //Validar valor do input name
-inputName.addEventListener("change", (e)=>{
+inputName.addEventListener("blur", (e)=>{
     let valor = e.target.value
     if(valor.length < 6){
         //valor incorreto
-       estilizarTnputIncorreto(inputName, nameHelper)  
+       estilizarInputIncorreto(inputName, nameHelper)  
        inputsCorretos.name = false   
     }else{
         //valor correto
-       estilizarTnputCorreto(inputName, nameHelper)
+       estilizarInputCorreto(inputName, nameHelper)
        inputsCorretos.name = true
     }
 });
@@ -55,28 +63,20 @@ inputName.addEventListener("change", (e)=>{
 
 mostrarPopup(inputEmail, emailLabel)
 
-inputEmail.addEventListener("change", (e)=>{
+inputEmail.addEventListener("blur", (e)=>{
     let valor = e.target.value
     if(valor.includes("@") && valor.includes(".com")){
         //Email valido
-        estilizarTnputCorreto(inputEmail, emailHelper)
+        estilizarInputCorreto(inputEmail, emailHelper)
         inputsCorretos.email = true
     }else{
         //Email incorreto
-        estilizarTnputIncorreto(inputEmail, emaillHelper)
+        estilizarInputIncorreto(inputEmail, emaillHelper)
         inputsCorretos.email = false
     }
 })
 
 //---------- evitar envio de formulario ----------//
-
-//variaveis Submit/inputs para button
-let btnSubmit = document.querySelector('button[type="submit"]');
-let inputsCorretos = {
-    name: false,
-    email: false
-}
-
 
 
 btnSubmit.addEventListener("click", (e) => {
