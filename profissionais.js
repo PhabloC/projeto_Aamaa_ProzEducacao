8c80e4815354fae084b853c94e8743667c6bff2f
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-11-23 14:04:21 -0300

    colocação da ordem de chamada

--- profissionais.js
+++ profissionais.js
@@ -1,6 +1,19 @@
-
-let inputNumero = document.getElementById("iNumero");
-
+//Variaveis name
+let inputName = document.getElementById("iName");
+let nameLabel = document.querySelector('label[for="name"]');
+let nameHelper = document.getElementById("nameHelper");
+//variaveis email
+let inputEmail = document.getElementById("iEmail");
+let emailLabel = document.querySelector('label[for="email"]');
+let emaillHelper = document.getElementById("emailHelper");
+//variaveis telefone
+let inputNumero = document.getElementById("iTelefone");
+//variaveis Submit/inputs para button
+let btnSubmit = document.querySelector('button[type="submit]');
+let inputsCorretos = {
+    name: false,
+    email: false
+}
 
 function mostrarPopup(input, label){
         //Mostrar o popup de campo obrigatório
@@ -26,9 +39,7 @@ function estilizarTnputIncorreto(input, helper){
 }
 
 //---------- validaçao nome ----------//
-let inputName = document.getElementById("iName");
-let nameLabel = document.querySelector('label[for="name"]');
-let nameHelper = document.getElementById("nameHelper")
+
 
 mostrarPopup(inputName, nameLabel)
 
@@ -48,9 +59,7 @@ inputName.addEventListener("change", (e)=>{
 
 // ---------Validação de e-mail --------- //
 
-let inputEmail = document.getElementById("iEmail");
-let emailLabel = document.querySelector('label[for="email"]');
-let emaillHelper = document.getElementById("emailHelper");
+
 
 mostrarPopup(inputEmail, emailLabel)
 
@@ -58,29 +67,25 @@ inputEmail.addEventListener("change", (e)=>{
     let valor = e.target.value
     if(valor.includes("@") && valor.includes(".com")){
         //Email valido
-        estilizarTnputCorreto(inputName, nameHelper)
+        estilizarTnputCorreto(inputEmail, emailHelper)
         inputsCorretos.email = true
     }else{
         //Email incorreto
-        estilizarTnputIncorreto(inputName, nameHelper)
+        estilizarTnputIncorreto(inputEmail, emaillHelper)
         inputsCorretos.email = false
     }
 })
 
 //---------- evitar envio de formulario ----------//
 
-let btnSubmit = document.querySelector('button[type="submit]');
-let inputsCorretos = {
-    name: false,
-    email: false
-}
+
 
 btnSubmit.addEventListener("click", () => {
     if(inputsCorretos.name == false ||
         inputsCorretos.email == false){
         e.preventDefault()
-        alert("Os campos obrigatórios precisam ser preenchidos corretamente")
+        alert("Os campos obrigatórios precisam ser preenchidos corretamente");
     }else{
-        alert("Formulário enviado com sucesso")
+        alert("Formulário enviado com sucesso");
     }
 });
