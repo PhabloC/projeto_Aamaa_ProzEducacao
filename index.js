1097a2faadf714cb950f09866a2038fd4a0dc215
Phablo Carvalho <phablocarvalho14@hotmail.com>
2023-11-23 20:12:29 -0300

    Adicionando o index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const donationButton = document.querySelector(".caixa-principal2 button");
+
+  donationButton.addEventListener("click", function () {
+    // window.location.href = "pagina-de-doacao.html";
+    alert("Redirecionando para a página de doação...");
+  });
+});
