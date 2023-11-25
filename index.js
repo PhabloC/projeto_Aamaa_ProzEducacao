3d26c6eb20ea79df8c5d1dce7b079a92d5b372d2
Phablo Carvalho <phablocarvalho14@hotmail.com>
2023-11-25 15:42:17 -0300

    Adicionando responsividade no cabeçalho do index

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
     alert("Redirecionando para a página de doação...");
   });
 });
+
+function menuShow() {
+  let menuMobile = document.querySelector(".mobile-menu");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    document.querySelector(".icon").src = "assets/img/icon-menu.svg";
+  } else {
+    menuMobile.classList.add("open");
+    document.querySelector(".icon").src = "assets/img/x.svg";
+  }
+  let logoBranco = document.querySelector(".logo-branco");
+  logoBranco.style.width = "50px"; // Defina o tamanho desejado
+
+  // Adicionar código para diminuir o tamanho do botão e remover o gradiente
+  let mobileMenuIconBtn = document.querySelector(".mobile-menu-icon button");
+  mobileMenuIconBtn.style.fontSize = "10px"; // Ajuste o tamanho da fonte conforme necessário
+  mobileMenuIconBtn.style.background = "transparent"; // Remove o fundo gradiente
+}
