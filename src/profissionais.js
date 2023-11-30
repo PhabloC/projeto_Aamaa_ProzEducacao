b7ab593e85bbdcb8bb6368db3b0c98c4ada8167f
Phablo Carvalho <phablocarvalho14@hotmail.com>
2023-11-30 15:33:16 -0300

    Adicionando responsividade

--- src/profissionais.js
+++ src/profissionais.js
@@ -10,114 +10,110 @@ let inputEmailPessoa = document.getElementById("iEmailPessoa");
 let inputEmailProfissional = document.getElementById("iEmailProfissional");
 let emailLabel = document.querySelector('label[for="email"]');
 let emaillHelperPessoa = document.getElementById("emailHelperPessoa");
-let emaillHelperProfissional = document.getElementById("emailHelperProfissional");
+let emaillHelperProfissional = document.getElementById(
+  "emailHelperProfissional"
+);
 
 //variaveis Submit/inputs para button
 let btnSubmit = document.querySelector('button[type="submit"]');
 let inputsCorretos = {
-    name: true,
-    email: true
-}
+  name: true,
+  email: true,
+};
 
-function mostrarPopup(input, label){
-        //Mostrar o popup de campo obrigatório
-    input.addEventListener("focus", ()=>{
-        label.classList.add("requiredPopup")
-    });
+function mostrarPopup(input, label) {
+  //Mostrar o popup de campo obrigatório
+  input.addEventListener("focus", () => {
+    label.classList.add("requiredPopup");
+  });
 
-    //Ocultar popup de campo obrigatório
-    input.addEventListener("blur", ()=>{
-        label.classList.remove("requiredPopup")
-    });
-
+  //Ocultar popup de campo obrigatório
+  input.addEventListener("blur", () => {
+    label.classList.remove("requiredPopup");
+  });
 }
-function estilizarInputCorreto(input, helper){
-    helper.classList.remove("visible");
-    input.classList.remove("error");
-    input.classList.add("correct");    
+function estilizarInputCorreto(input, helper) {
+  helper.classList.remove("visible");
+  input.classList.remove("error");
+  input.classList.add("correct");
 }
-function estilizarInputIncorreto(input, helper){
-    helper.classList.add("visible");
-    input.classList.add("error");
-    input.classList.remove("correct");    
+function estilizarInputIncorreto(input, helper) {
+  helper.classList.add("visible");
+  input.classList.add("error");
+  input.classList.remove("correct");
 }
 
 //---------- validaçao nome ----------//
 
-
-mostrarPopup(inputNamePessoa, nameLabel)
-
-//Validar valor do input name
-inputNamePessoa.addEventListener("blur", (e)=>{
-    let valor = e.target.value
-    if(valor.length < 6){
-        //valor incorreto
-       estilizarInputIncorreto(inputNamePessoa, nameHelperPessoa)  
-       inputsCorretos.name = false   
-    }else{
-        //valor correto
-       estilizarInputCorreto(inputNamePessoa, nameHelperPessoa)
-       inputsCorretos.name = true
-    }
-});
-mostrarPopup(inputNameProfissional, nameLabel)
+mostrarPopup(inputNamePessoa, nameLabel);
 
 //Validar valor do input name
-inputNameProfissional.addEventListener("blur", (e)=>{
-    let valor = e.target.value
-    if(valor.length < 6){
-        //valor incorreto
-       estilizarInputIncorreto(inputNameProfissional, nameHelperProfissional)  
-       inputsCorretos.name = false   
-    }else{
-        //valor correto
-       estilizarInputCorreto(inputNameProfissional, nameHelperProfissional)
-       inputsCorretos.name = true
-    }
+inputNamePessoa.addEventListener("blur", (e) => {
+  let valor = e.target.value;
+  if (valor.length < 6) {
+    //valor incorreto
+    estilizarInputIncorreto(inputNamePessoa, nameHelperPessoa);
+    inputsCorretos.name = false;
+  } else {
+    //valor correto
+    estilizarInputCorreto(inputNamePessoa, nameHelperPessoa);
+    inputsCorretos.name = true;
+  }
 });
+mostrarPopup(inputNameProfissional, nameLabel);
 
+//Validar valor do input name
+inputNameProfissional.addEventListener("blur", (e) => {
+  let valor = e.target.value;
+  if (valor.length < 6) {
+    //valor incorreto
+    estilizarInputIncorreto(inputNameProfissional, nameHelperProfissional);
+    inputsCorretos.name = false;
+  } else {
+    //valor correto
+    estilizarInputCorreto(inputNameProfissional, nameHelperProfissional);
+    inputsCorretos.name = true;
+  }
+});
 
 // ---------Validação de e-mail --------- //
 
+mostrarPopup(inputEmailPessoa, emailLabel);
 
+inputEmailPessoa.addEventListener("blur", (e) => {
+  let valor = e.target.value;
+  if (valor.includes("@") && valor.includes(".com")) {
+    //Email valido
+    estilizarInputCorreto(inputEmailPessoa, emailHelperPessoa);
+    inputsCorretos.email = true;
+  } else {
+    //Email incorreto
+    estilizarInputIncorreto(inputEmailPessoa, emaillHelperPessoa);
+    inputsCorretos.email = false;
+  }
+});
+mostrarPopup(inputEmailProfissional, emailLabel);
 
-mostrarPopup(inputEmailPessoa, emailLabel)
-
-inputEmailPessoa.addEventListener("blur", (e)=>{
-    let valor = e.target.value
-    if(valor.includes("@") && valor.includes(".com")){
-        //Email valido
-        estilizarInputCorreto(inputEmailPessoa, emailHelperPessoa)
-        inputsCorretos.email = true
-    }else{
-        //Email incorreto
-        estilizarInputIncorreto(inputEmailPessoa, emaillHelperPessoa)
-        inputsCorretos.email = false
-    }
-})
-mostrarPopup(inputEmailProfissional, emailLabel)
-
-inputEmailProfissional.addEventListener("blur", (e)=>{
-    let valor = e.target.value
-    if(valor.includes("@") && valor.includes(".com")){
-        //Email valido
-        estilizarInputCorreto(inputEmailProfissional, emaillHelperProfissional)
-        inputsCorretos.email = true
-    }else{
-        //Email incorreto
-        estilizarInputIncorreto(inputEmailProfissional, emaillHelperProfissional)
-        inputsCorretos.email = false
-    }
-})
+inputEmailProfissional.addEventListener("blur", (e) => {
+  let valor = e.target.value;
+  if (valor.includes("@") && valor.includes(".com")) {
+    //Email valido
+    estilizarInputCorreto(inputEmailProfissional, emaillHelperProfissional);
+    inputsCorretos.email = true;
+  } else {
+    //Email incorreto
+    estilizarInputIncorreto(inputEmailProfissional, emaillHelperProfissional);
+    inputsCorretos.email = false;
+  }
+});
 
 //---------- evitar envio de formulario ----------//
 
-
 btnSubmit.addEventListener("click", (e) => {
-    if(inputsCorretos.name == false || inputsCorretos.email == false){
-        e.preventDefault()
-        alert("Os campos obrigatórios precisam ser preenchidos corretamente")
-    }else{
-        alert("Formulário enviado com sucesso")
-    }
+  if (inputsCorretos.name == false || inputsCorretos.email == false) {
+    e.preventDefault();
+    alert("Os campos obrigatórios precisam ser preenchidos corretamente");
+  } else {
+    alert("Formulário enviado com sucesso");
+  }
 });
