81dd3148e62743903706282d4b8fb7d05fb2e9bb
Cristiano Agnaldo Mulinari <62948211+Crismulinari@users.noreply.github.com>
2023-12-04 15:27:06 -0300

    primeira tentativa de ordem

--- src/profissionais.js
+++ src/profissionais.js
@@ -111,15 +111,16 @@ inputEmailProfissional.addEventListener("blur", (e) => {
 //---------- evitar envio de formulario ----------//
 
 //<<<<<<< HEAD
-btnSubmit.addEventListener("click", (e) => {
-  if (inputsCorretos.name == false || inputsCorretos.email == false) {
-    e.preventDefault();
-    alert("Os campos obrigatórios precisam ser preenchidos corretamente");
-  } else {
-    alert("Formulário enviado com sucesso");
-  }
-});
+
+
+
+
+
+
 //=======
+
+
+
 
 form1.addEventListener("click", (e) => {
     if(inputsCorretos.name == false || 
@@ -143,4 +144,8 @@ form2.addEventListener("click", (e) => {
         alert("Formulário enviado com sucesso")
     }
 });
+
+
+
+
 //>>>>>>> 0f37101b91b4c7c473e78eb07d5a47439db5724d
